a0b6f7735274c49515c01610bb112cf3e35bd97e
Andreas Ziegler <42463934+Andreas-Ziegler22@users.noreply.github.com>
2023-06-26 08:16:15 +0000

    update h1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-function Home(){
-    return <h1>Filipe Dechamps voce é o cara!</h1>
-        
-    
+function Home() {
+  return (
+    <h1>
+      your future deppends only by yourself great things will happen with hard
+      working{" "}
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
